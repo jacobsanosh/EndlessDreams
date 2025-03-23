9dd206413b5f9ed06274272500a775d815fcc3b0
JacobSanosh <jacobsanosh@gmail.com>
2025-03-23 15:54:37 +0530

    setting navigation part

--- src/Layouts/HomeLayout.tsx
+++ src/Layouts/HomeLayout.tsx
@@ -1,9 +1,11 @@
 import LandingPage from "./components/LandingPage";
+import Navbar from "./components/Navbar";
 
 const HomeLayout = () => {
   return (
-    <div id="home-layout" className="w-ful h-full flex flex-col">
+    <div id="home-layout" className="w-ful h-full flex flex-col relative">
       <LandingPage />
+      <Navbar />
     </div>
   );
 };
