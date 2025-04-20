3f615f7751b7aa02d3101d9688b7556a32412e33
JacobSanosh <jacobsanosh@gmail.com>
2025-04-20 15:43:06 +0530

    making the testimonial responsive

--- src/Layouts/components/Testimonial.tsx
+++ src/Layouts/components/Testimonial.tsx
@@ -1,3 +1,5 @@
+import Marquee from "react-fast-marquee";
+
 const Testimonial = () => {
   return (
     <div className="w-4/5 flex flex-col m-auto mt-10 gap-10 mb-10 text-black">
@@ -8,23 +10,32 @@ const Testimonial = () => {
           Dreams Builders & Developers.
         </p>
       </div>
-      <div className="w-full flex justify-between text-xl">
-        <div className="max-w-sm rounded-2xl overflow-hidden shadow-2xl p-8 h-[400px] flex justify-center items-center">
-          "Endless Dreams Builders & Developers exceeded our expectations with
-          their impeccable craftsmanship and commitment to delivering on time."
-        </div>
-        <div className="max-w-sm rounded-2xl overflow-hidden shadow-2xl p-8 h-[400px] flex justify-center items-center">
-          "Working with the Endless Dreams team was a pleasure as they brought
-          our vision to life with exceptional attention to detail and
-          professionalism."
-        </div>
-        <div className="max-w-sm rounded-2xl overflow-hidden shadow-2xl p-8 h-[400px] flex justify-center items-center">
-          "The Endless Dreams team's dedication to quality and client
-          satisfaction sets them apart as a trusted partner for our construction
-          projects."
-        </div>
+      <div className="w-full text-xl">
+        <Marquee
+          direction="left"
+          speed={50}
+          pauseOnHover={true}
+          className="p-5"
+        >
+          <div className="text-sm w-[15rem] h-[15rem] sm:w-[16rem] sm:h-[16rem] md:w-[20rem] md:h-[20rem] rounded-2xl overflow-hidden shadow-xl p-8 flex justify-center items-center mx-8">
+            "Endless Dreams Builders & Developers exceeded our expectations with
+            their impeccable craftsmanship and commitment to delivering on
+            time."
+          </div>
+          <div className="text-sm w-[15rem] h-[15rem] sm:w-[16rem] sm:h-[16rem] md:w-[20rem] md:h-[20rem] rounded-2xl overflow-hidden shadow-xl p-8 flex justify-center items-center mx-8">
+            "Working with the Endless Dreams team was a pleasure as they brought
+            our vision to life with exceptional attention to detail and
+            professionalism."
+          </div>
+          <div className="text-sm w-[15rem] h-[15rem] sm:w-[16rem] sm:h-[16rem] md:w-[20rem] md:h-[20rem] rounded-2xl overflow-hidden shadow-xl p-8 flex justify-center items-center mx-8">
+            "The Endless Dreams team's dedication to quality and client
+            satisfaction sets them apart as a trusted partner for our
+            construction projects."
+          </div>
+        </Marquee>
       </div>
     </div>
   );
 };
+
 export default Testimonial;
